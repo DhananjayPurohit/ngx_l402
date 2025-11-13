f462963f74386768c094bd9f5311318b5b80185f
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2025-11-13 15:12:41 +0000

    chore: add contributing.md and remove unnecessary github secrets

--- src/cashu_redemption_logger.rs
+++ src/cashu_redemption_logger.rs
@@ -1,5 +1,5 @@
-use std::io::Write;
 use log::error;
+use std::io::Write;
 
 const LOG_FILE_PATH: &str = "/var/log/nginx/cashu_redemption.log";
 
@@ -9,16 +9,19 @@ pub fn log_redemption(msg: &str) {
     match std::fs::OpenOptions::new()
         .create(true)
         .append(true)
-        .open(LOG_FILE_PATH) {
+        .open(LOG_FILE_PATH)
+    {
         Ok(mut file) => {
             let timestamp = chrono::Local::now().format("%Y-%m-%d %H:%M:%S");
             if let Err(e) = writeln!(file, "[{}] {}", timestamp, msg) {
                 error!("Failed to write to cashu redemption log: {}", e);
             }
-        },
+        }
         Err(e) => {
-            error!("Failed to open cashu redemption log file {}: {}", LOG_FILE_PATH, e);
+            error!(
+                "Failed to open cashu redemption log file {}: {}",
+                LOG_FILE_PATH, e
+            );
         }
     }
 }
-
