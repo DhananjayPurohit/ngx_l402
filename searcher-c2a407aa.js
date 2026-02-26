1d1f4c4e5f96714e64523a38e9b782fecaca5f7d
DhananjayPurohit <DhananjayPurohit@users.noreply.github.com>
2026-02-26 05:39:28 +0000

    deploy: c181fb79eff1a429192e502ce4ddcb971f760e8a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-66b9be82.js',
+                path_to_root + 'searchindex-0956015c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
